bab4c9e6fdacb7b552cd2604ae45e5e1792fb307
James Stolhammer <jimstolhammer@gmail.com>
2022-08-24 13:05:08 -0700

    Demo solution for CSS conflicts

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import Joey from './Joey.jsx';
+import Chandler from './Chandler.jsx';
+import Ross from './Ross.jsx';
+import Rachel from './Rachel.jsx';
 
 let App = (props) => (
-  <p>Hello, world</p>
+  <div>
+    <Joey />
+    <Chandler />
+    <Ross />
+    <Rachel />
+  </div>
 )
 
 export default App;
---
+++ client/src/components/Rachel.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+let Rachel = (props) => (
+  <div className="rachel">
+    <h1>Rachel</h1>
+    <p>Lorem ipsum dolor sit amet consectitur edipiscing elit.</p>
+    <div class="content">
+      <img src="https://www.pinkvilla.com/files/styles/amp_metadata_content_image/public/jennifer-anistons-character-rachel-green-in-friends-was-almost-replaced_.jpg" />
+      <ul>
+        <li id="thing">thing 1</li>
+        <li>thing 2</li>
+      </ul>
+    </div>
+  </div>
+)
+
+export default Rachel;
