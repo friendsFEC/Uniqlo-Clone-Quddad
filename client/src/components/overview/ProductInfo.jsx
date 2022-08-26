fc49e400ca46996e2804a04e3d0c9e7a96c6a488
Goksu Okar <goksuokar@gmail.com>
2022-08-26 12:14:10 -1000

    got config to work and fetching data from axios

--- client/src/components/overview/ProductInfo.jsx
+++ client/src/components/overview/ProductInfo.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import axios from 'axios';
+import config from '../../../../config.js';
+
 
 const ProductInfo = () => {
+  axios.get('/products/65631', {
+    baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/rfp/',
+    headers: {
+      'Authorization': config.TOKEN
+    }
+  }).then((res) => (console.log(res.data)));
   return (
     <div className="ov-infoBox">
       This will be the title
