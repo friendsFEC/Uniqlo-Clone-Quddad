7d041f6eee9c7a17f47e8b05d112abe567b481e0
Goksu Okar <goksuokar@gmail.com>
2022-08-26 13:27:12 -1000

    rendering description, title and image

--- client/src/components/overview/ProductInfo.jsx
+++ client/src/components/overview/ProductInfo.jsx
@@ -3,34 +3,12 @@ import axios from 'axios';
 import config from '../../../../config.js';
 
 
-class ProductInfo extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      product: {}
-    }
-  }
-
-  componentDidMount() {
-    this.fetch();
-  }
-
-  fetch() {
-    axios.get('/products/65631', {
-      baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/rfp/',
-      headers: {
-        'Authorization': config.API_KEY
-      }
-    }).then((res) => (this.setState({product: res.data})));
-  }
-
-  render () {
+const ProductInfo = ({ product }) => {
     return (
       <div className="ov-infoBox">
-        {this.state.product.name}
+        {product.name}
       </div>
     )
-  }
 }
 
 
