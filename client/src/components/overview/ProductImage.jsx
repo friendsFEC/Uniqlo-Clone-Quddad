56563a9782bdcd22b6b041f8b05c144777f85e0c
Goksu Okar <goksuokar@gmail.com>
2022-08-27 18:21:38 -1000

    more styling for imagebox

--- client/src/components/overview/ProductImage.jsx
+++ client/src/components/overview/ProductImage.jsx
@@ -3,7 +3,10 @@ import React from 'react';
 const ProductImage = ({ style }) => {
   return (
     <div className="ov-imageBox">
+      <div className="ov-imageBox_carousel">c/na/nr/no/nu/ns/ne/nl</div>
+      <div>
       <img id="ov-mainImage" src={style.photos[0].url}/>
+      </div>
     </div>
   )
 }
