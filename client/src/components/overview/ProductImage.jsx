c78e56a6e2353b010de5ca93bbc0803e4478602a
Goksu Okar <goksuokar@gmail.com>
2022-08-28 21:09:25 -1000

    implemented image slider with prev and next buttons on main image box

--- client/src/components/overview/ProductImage.jsx
+++ client/src/components/overview/ProductImage.jsx
@@ -1,10 +1,30 @@
 import React from 'react';
+import { useState } from 'react';
+import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 
 const ProductImage = ({ photosData }) => {
+  const [current, setCurrent] = useState(0);
+  const length = photosData.length;
+
+  const changeToNext = () => {
+    setCurrent(current === length - 1 ? 0 : current + 1);
+  }
+
+  const changeToPrev = () => {
+    setCurrent(current === 0 ? length - 1 : current - 1);
+  }
+
   return (
     <div className="ov-imageBox">
-      <div className="ov-imageBox_carousel">c/na/nr/no/nu/ns/ne/nl</div>
-      {photosData.map((photo) => <img src={photo.url}/>)}
+      <AiOutlineArrowLeft className="ov-imageBox_prev" onClick={changeToPrev}/>
+      <AiOutlineArrowRight className="ov-imageBox_next" onClick={changeToNext}/>
+      {photosData.map((photo, index) => {
+        return (
+          <div key={index} className={index === current ? 'ov-imageBox_activeSlide' : 'ov-imageBox_slide'}>
+            {index === current && <img className="ov-imageBox_mainImage"src={photo.url}/>}
+          </div>
+        )
+      })}
     </div>
   )
 }
