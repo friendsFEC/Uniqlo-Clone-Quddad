070507cfde0ee34783ef2894350015eb13825853
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-08-29 20:10:44 -0700

    Implemented the QA displaying feature
    The questions and their corresponding answers within this list will be displayed in an expanding and collapsing accordion. By default, on page load up to four questions should be displayed. Up to two answers should display for each question. The remaining questions or answers should be hidden until the user loads them using the “More Answered Questions” button (section 1.3.4).
    
    + Added a personal utility file to do customized tasks:
    a. turn an object of objects (where objects are similar) to an array of objects
    b. topXItems to create an array that has a chosen number of items from an array
    
    + Installed Underscore

--- client/src/components/QA components/ListOfQA.jsx
+++ client/src/components/QA components/ListOfQA.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-import QAItem from './QaItem.jsx';
+import QAItem from './QAItem.jsx';
+const _V = require('../Utility/V.jsx');
 
 let ListOfQA = (props) => {
+  const topFourQuestions = _V.topXItems(4, props.chosenProduct);
   return (
     <div id="qa-ListOfQA">
     {/*
     1. display questions
     2. display answers */}
-    {props.chosenProduct.map((item) => {
+    {topFourQuestions.map((item) => {
       return <QAItem key={item.question_id} questionAnswer={item} />
     })}
     </div>
