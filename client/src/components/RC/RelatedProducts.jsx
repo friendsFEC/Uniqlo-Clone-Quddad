fe1210ea46e05981b0ff1c9b29e592386227c10e
Nick <nicolastiennguyen@gmail.com>
2022-08-30 12:41:07 -0700

    component now uses hooks

--- client/src/components/RC/RelatedProducts.jsx
+++ client/src/components/RC/RelatedProducts.jsx
@@ -2,21 +2,33 @@ import React from 'react';
 import axios from 'axios';
 import RelatedProductsEntry from './RelatedProductsEntry.jsx'
 import config from '../../../../config.js'
+import { AiOutlineStar } from 'react-icons/ai';
 
-class RelatedProducts extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {}
-  }
+const RelatedProducts = (props) => {
 
-  render() {
+  const createRPCard = () => {
     return (
       <div>
-        <h3 className = "rc-title"> Related Products </h3>
-        {this.props.createRPCard()}
-      </div>
+      {props.RPInfo.map(product => {
+        return <div className = "rc-main" key = {product.id}>
+          <div className = "rc-small-titles">
+          <button className = "rc-rp-button"><AiOutlineStar/></button>
+          <p>{product.category}</p>
+          <p>{product.name}</p>
+          <p>{product.default_price}</p>
+          </div>
+        </div>
+      })}
+    </div>
     )
   }
+
+  return (
+    <div>
+      <h3 className = "rc-title"> Related Products </h3>
+      {createRPCard()}
+    </div>
+  )
 }
 
 export default RelatedProducts;
--- client/src/components/RelatedAndComparison.jsx
+++ client/src/components/RelatedAndComparison.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import axios from 'axios';
 import RelatedProducts from './RC/RelatedProducts.jsx'
 import YourOutfit from './RC/YourOutfit.jsx'
@@ -6,142 +7,68 @@ import config from '../../../config.js'
 import { AiOutlineStar } from 'react-icons/ai';
 import Modal from './RC/Modal.jsx'
 
-class RelatedAndComparison extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      relatedProductInfo: [],
-      relatedProductStyles: [],
-      defaultProductStyles: [],
-      review: 0,
-      modalStatus: false
-    }
-    this.createRPCard = this.createRPCard.bind(this);
-    this.getImage = this.getImage.bind(this);
-    this.handleModal = this.handleModal.bind(this);
-    // this.getReview = this.getReview.bind(this);
-  }
+const RelatedAndComparison = () => {
+  const [RPInfo, setRPInfo] = useState([]);
+  const [RPStyles, setRPStyles] = useState([]);
+  const [DPStyles, setDPStyles] = useState([]);
 
-  componentDidMount() {
-    // get related productIDs of current product
-    axios
-      .get('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/65631/related', {
-        headers: {
-          Authorization: config.API_KEY,
-          'Content-Type': 'application/json'
-        }
+  const getRelatedInfo = () => {
+    axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/65631/related', {
+      headers: {
+        Authorization: config.API_KEY,
+        'Content-Type': 'application/json'
+      }
+    })
+    .then(res => {
+      const dataStorage = [];
+      const styleStorage = [];
+      res.data.forEach(id => {
+        axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${id}`, {
+          headers: {
+            Authorization: config.API_KEY,
+            'Content-Type': 'application/json'
+          }
+        })
+        .then(res => dataStorage.push(res.data))
+        .then(() => setRPInfo(dataStorage))
+        .catch(err => console.log(err))
       })
-      // for each ID, get it's info and store it in state
-      .then(res => {
-        const dataStorage = [];
-        const styleStorage = [];
-        res.data.forEach(id => {
-          axios
-            .get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${id}`, {
-            headers: {
+      res.data.forEach(id => {
+        axios.get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${id}/styles`, {
+          headers: {
               Authorization: config.API_KEY,
               'Content-Type': 'application/json'
+          }
+        })
+        .then(res => styleStorage.push(res.data.results))
+        .then(() => setRPStyles(styleStorage))
+        .then(() => {
+          const defaultProducts = [];
+          for (let i = 0; i < RPStyles.length; i++) {
+            for (let j = 0; j < RPStyles.length; j++) {
+              if (RPStyles[i][j]["default?"]) {
+                defaultProducts.push(RPStyles[i][j]);
+              }
             }
-          })
-          .then(res => dataStorage.push(res.data))
-          .then(() => this.setState({relatedProductInfo:dataStorage}))
-          .catch(err => console.log(err))
+          }
+        setDPStyles(defaultProducts);
         })
-        res.data.forEach(id => {
-          axios
-            .get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/products/${id}/styles`, {
-              headers: {
-                Authorization: config.API_KEY,
-                'Content-Type': 'application/json'
-              }
-            })
-            // .then(res => console.log(res.data))
-            .then(res => styleStorage.push(res.data.results))
-            .then(() => this.setState({relatedProductStyles:styleStorage}))
-            .then(() => {
-              const defaultProducts = [];
-              for (let i = 0; i < this.state.relatedProductStyles.length; i++) {
-                for (let j = 0; j < this.state.relatedProductStyles.length; j++) {
-                  if (this.state.relatedProductStyles[i][j]["default?"]) {
-                    defaultProducts.push(this.state.relatedProductStyles[i][j])
-                  }
-                }
-              }
-              this.setState({defaultProductStyles: defaultProducts})
-            })
-            .catch(err => console.log(err))
-        })
+        .catch(err => console.log(err))
       })
-      .catch(err => console.log('error getting related product IDs & info: ', err))
+    })
+    .catch(err => console.log(err))
   }
 
-  // handleModal(e) {
-  //   e.preventDefault()
-  //   this.setState({modalStatus: true})
-  //   console.log('working')
-  //   if (!this.state.modalStatus) return null
-  //   return (
-  //     <Modal></Modal>
-  //   )
-  // }
+  useEffect(() => {
+    getRelatedInfo()
+  }, [])
 
-  createRPCard() {
-    return (
-      <div>
-      {this.state.relatedProductInfo.map(product => {
-        return <div className = "rc-main" key = {product.id}>
-          {/* {this.getImage()} */}
-          <div className = "rc-small-titles">
-          <button className = "rc-rp-button" onClick = {this.handleModal}><AiOutlineStar/></button>
-          {/* <Modal modalStatus={this.state.modalStatus}></Modal> */}
-          <p>{product.category}</p>
-          <p>{product.name}</p>
-          <p>{product.default_price}</p>
-          {/* <div>{this.getReview(product.id)}</div> */}
-          </div>
-        </div>
-      })}
+  return (
+    <div>
+    <RelatedProducts RPInfo = {RPInfo}/>
+    <YourOutfit />
     </div>
-    )
-  }
-
-  getImage() {
-    return (
-      this.state.defaultProductStyles.map(style => {
-        return <div>hey</div>
-      })
-    )
-  }
-
-  // getReview(id) {
-  //   return axios
-  //   .get(`https://app-hrsei-api.herokuapp.com/api/fec2/rfp/reviews?product_id=${id}`, {
-  //     headers: {
-  //       Authorization: config.API_KEY,
-  //       'Content-Type': 'application/json'
-  //     }
-  //   })
-  //   .then(response => {window.datas = response.data; return response.data})
-  //   .then(data =>
-  //     data.results.reduce((memo, review) => memo + review.rating, 0) / data.count)
-  //     .then(averageReview => <div>{averageReview}</div>)
-  //     .catch(err => console.log('Error fetching data:', err));
-  // }
-
-
-  render() {
-    // console.log('relatedProductInfo:', this.state.relatedProductInfo)
-    // console.log('relatedProductStyles: ', this.state.relatedProductStyles)
-    // console.log('defaultProductStyles: ', this.state.defaultProductStyles)
-    return (
-      <div>
-      <RelatedProducts
-        createRPCard = {this.createRPCard}
-      />
-      <YourOutfit />
-      </div>
-    )
-  }
+  )
 }
 
 export default RelatedAndComparison;
