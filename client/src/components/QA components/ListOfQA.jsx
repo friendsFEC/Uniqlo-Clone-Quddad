19dab8b31dba9429584447230b7b0d9a6b2326c1
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-08-31 16:48:11 -0700

    Update ListOfQA.jsx
    stateless ListOfQa => stateful ListOfQa

--- client/src/components/QA components/ListOfQA.jsx
+++ client/src/components/QA components/ListOfQA.jsx
@@ -3,25 +3,30 @@ import QAItem from './QAItem.jsx';
 import Filter from './Filter.jsx';
 const _V = require('../Utility/V.jsx');
 
-let ListOfQA = (props) => {
-  const filteredQuestions = props.chosenProduct.filter((question) =>
-    question.question_helpfulness > 0
-  );
-  const topFourQuestions = _V.topXItems(4, filteredQuestions);
+class ListOfQA extends React.Component {
+  constructor(props) {
+    super(props);
+  }
 
+  render() {
+    const filteredQuestions = this.props.chosenProduct.filter((question) =>
+      question.question_helpfulness > 0
+    );
+    const topFourQuestions = _V.topXItems(4, filteredQuestions);
 
-  return (
-    <div id="qa-ListOfQA">
+    return (
+      <div id="qa-ListOfQA">
 
-    <Filter />
-    {/*
-    1. display questions
-    2. display answers */}
-    {topFourQuestions.map((item) => {
-      return <QAItem key={item.question_id} questionAnswer={item} />
-    })}
-    </div>
-  )
-};
+      <Filter />
+      {/*
+      1. display questions
+      2. display answers */}
+      {topFourQuestions.map((item) => {
+        return <QAItem key={item.question_id} questionAnswer={item} />
+      })}
+      </div>
+    )
+  };
 
+}
 export default ListOfQA;
