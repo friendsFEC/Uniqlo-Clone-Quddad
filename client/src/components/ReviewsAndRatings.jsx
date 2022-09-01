91900ff0e922173a1e3f08f374b5bda7d7760c3a
James Stolhammer <jimstolhammer@gmail.com>
2022-09-01 00:41:11 -0700

    Applied fixes bringing review section closer to BRD.
    * Some style changes
    * Added :hover styles on rating filters
    * Added some CSS and Javascript magic on the review button
    * Got the content portion of the review form written
    * Still need to script the form

--- client/src/components/ReviewsAndRatings.jsx
+++ client/src/components/ReviewsAndRatings.jsx
@@ -19,6 +19,7 @@ let ReviewsAndRatings = (props) => {
   const [more, setMore] = useState(true);
   const [search, setSearch] = useState('');
   const [ratingFilter, setRatingFilter] = useState([]);
+  const [helpfulReviews, setHelpfulReviews] = useState([]);
   const count = 2;
 
   /* utility functions */
@@ -26,21 +27,27 @@ let ReviewsAndRatings = (props) => {
     let average = 0;
     if (meta.ratings) {
       let sum = 0;
-      for (let i=1; i <= 5; i++) {
-        sum += (parseInt(meta.ratings[i]) * i);
+      for (let k in meta.ratings) {
+        sum += (parseInt(meta.ratings[k]) * k);
       }
+      //console.log(sum, total);
       average = sum / total;
     } else {
+      //console.log('meta.ratings undefined:', meta);
     }
+    //console.log('calcd average:', average);
     return average;
   };
   const calculateTotal = () => {
     let total = 0;
+    //console.log('calc total, meta.ratings:', meta.ratings);
     if (meta.ratings) {
-      for (let i=1; i <= 5; i++) {
-        total += parseInt(meta.ratings[i]);
+      let keys = Object.keys(meta.ratings);
+      for (let k of keys) {
+        total += parseInt(meta.ratings[k]);
       }
     }
+    //console.log('calcd total:', total);
     return total;
   }
 
@@ -93,6 +100,25 @@ let ReviewsAndRatings = (props) => {
     }
   }
 
+  const markHelpful = (review_id) => {
+    return axios.put(`${serverURL}/${review_id}/helpful`,{}, {
+      headers: {
+        Authorization: config.API_KEY,
+        'Access-Control-Allow-Origin': '*'
+      }
+    })
+    .then(res => res)
+    .catch(err => console.log('Error marking review as helpful:', err));
+  }
+  const reportReview = (review_id) => {
+    axios.put(`${serverURL}/${review_id}/report`, {}, {
+      headers: {
+        Authorization: config.API_KEY,
+        'Access-Control-Allow-Origin': '*'
+      }
+    }).catch(err => console.log('Error reporting a review:', err));
+  }
+
   const searchFilter = (review) => {
     let keys = ['summary', 'body', 'reviewer_name']
     let re = RegExp(search.toLowerCase());
@@ -123,12 +149,14 @@ let ReviewsAndRatings = (props) => {
       let keys = Object.keys(meta.characteristics);
       keys.map((char, i) => {
         let arrow = document.getElementsByClassName('characteristics-arrow')[i];
-        let bar = arrow.parentElement;
-        let value = Number(meta.characteristics[char].value);
-        //let left = bar.offsetLeft + (bar.offsetWidth *  value / 5) - arrow.offsetWidth / 2;
-        let left = (bar.offsetWidth *  value / 5) - (arrow.width);
-        //console.log('value:', value, 'left padding:', left);
-        arrow.style.paddingLeft = left;
+        if (arrow) {
+          let bar = arrow.parentElement;
+          let value = Number(meta.characteristics[char].value);
+          //let left = bar.offsetLeft + (bar.offsetWidth *  value / 5) - arrow.offsetWidth / 2;
+          let left = (bar.offsetWidth *  value / 5) - (arrow.width);
+          //console.log('value:', value, 'left padding:', left);
+          arrow.style.paddingLeft = left;
+        }
       })
     }
   }
@@ -168,10 +196,12 @@ let ReviewsAndRatings = (props) => {
     let loadedReviews = Array.from(document.getElementsByClassName('review-tile'));
     let targetWidget = document.getElementById('rr-no-reviews-showing');
     let hiddenReviews = loadedReviews.filter(rev => rev.classList.contains('hidden'));
-    if (hiddenReviews.length === reviews.length) {
-      targetWidget.classList.contains('hidden') ? targetWidget.classList.toggle('hidden') : null;
-    } else {
-      targetWidget.classList.contains('hidden') ? null : targetWidget.classList.toggle('hidden');
+    if (targetWidget) {
+      if (hiddenReviews.length === reviews.length) {
+        targetWidget.classList.contains('hidden') ? targetWidget.classList.toggle('hidden') : null;
+      } else {
+        targetWidget.classList.contains('hidden') ? null : targetWidget.classList.toggle('hidden');
+      }
     }
   }, [ratingFilter, reviews, search]);
 
@@ -186,6 +216,7 @@ let ReviewsAndRatings = (props) => {
   return (
     <div className="rr" id="rr">
       <h1>Ratings & Reviews Section</h1>
+      {/*
       <p className="testing">
         <em onClick={() => console.log(reviews)}>This part is just for testing, I'll remove it
           once we tie everything together</em><br />
@@ -208,6 +239,7 @@ let ReviewsAndRatings = (props) => {
           onChange={({target}) => setProductID(target.value)}
         />
       </p>
+      */}
       <div className="review-grid">
         <SortOptions
           setSort={setSort}
@@ -221,9 +253,11 @@ let ReviewsAndRatings = (props) => {
           toggleFilter={toggleFilter}
           ratingFilter={ratingFilter}
           total={total}
+          reviews={reviews}
         />
         <ProductBreakDown
           meta={meta}
+          reviews={reviews}
         />
         <ReviewList
           more={more}
@@ -238,6 +272,11 @@ let ReviewsAndRatings = (props) => {
           ratingFilter={ratingFilter}
           formatDate={formatDate}
           debounce={debounce}
+          helpful={markHelpful}
+          setHelpful={setHelpfulReviews}
+          helpfulReviews={helpfulReviews}
+          total={total}
+          reportReview={reportReview}
         />
         <WriteReview />
       </div>
@@ -262,27 +301,44 @@ let ReviewList = (props) => {
     <div
       className="review-list"
       onScroll={({target}) => {
-			if ((target.scrollTop + target.clientHeight) === target.scrollHeight) {
-        retrieveReviews();
-			}}}
+        let btn = document.getElementById('rr-write-review-btn');
+        btn.classList.add('float');
+        //btn.style.top = window.innerHeight - target.offsetHeight;
+        btn.style.top = window.innerHeight - btn.offsetHeight * 2;
+        btn.style.left = target.offsetLeft;
+        btn.style.width = target.offsetWidth;
+        if (Math.abs((target.scrollTop + target.clientHeight) - target.scrollHeight) < 10) {
+          retrieveReviews();
+          btn.classList.remove('float');
+          btn.style.width = '';
+        } else if (target.scrollTop === 0) {
+          btn.classList.remove('float');
+        }
+      }}
     >
-      {props.reviews.map((review,i) =>
-      <ReviewTile
-        key={i}
-        review={review}
-        search={props.search}
-        searchFilter={props.searchFilter}
-        ratingFilter={props.ratingFilter}
-        formatDate={props.formatDate}
-      />)}
-      <p id="rr-no-reviews-showing">No reviews to show with selected filters! Try including more filters or load more reviews to see some results.</p>
+      {props.reviews.length ? props.reviews.map((review,i) => 
+        <ReviewTile
+          key={i}
+          review={review}
+          search={props.search}
+          searchFilter={props.searchFilter}
+          ratingFilter={props.ratingFilter}
+          formatDate={props.formatDate}
+          helpful={props.helpful}
+          setHelpful={props.setHelpful}
+          helpfulReviews={props.helpfulReviews}
+          total={props.total}
+          reportReview={props.reportReview}
+        />)
+       : <p>There are no reviews to display</p> }
+      {props.reviews.length ? <p id="rr-no-reviews-showing">No reviews to show with selected filters! Try including more filters or load more reviews to see some results.</p> : ''}
       { props.more ? (
         <button onClick={() => {
           props.getReviews((props.reviews.length / props.count) + 1, props.count, props.sort)
             .then(data => props.setReviews(props.reviews.concat(data.results)));
         }}>More Reviews</button> // add infinite scroll later on
       ) : '' }
-      <button onClick={() => document.getElementsByClassName('write-review')[0].classList.toggle('hidden')}>Add a review</button>
+      <div id='rr-write-review-btn'><button  onClick={() => document.getElementsByClassName('write-review')[0].classList.toggle('hidden')}>Add a review</button></div>
     </div>
   )
 };
@@ -305,7 +361,8 @@ let ReviewTile = (props) => {
       <div>
         <span className="star-rating"><StarRating rating={props.review.rating} /></span>
         <div className="reviewer-and-date">
-           [need to verify purchaser] {props.review.reviewer_name}, {props.formatDate(props.review.date)}
+          {/* there is no way to verify if user_id / email  is associated with a purchase */}
+           {props.review.reviewer_name}, {props.formatDate(props.review.date)}
         </div>
       </div>
       <p className="bold">{props.review.summary}</p>
@@ -343,7 +400,30 @@ let ReviewTile = (props) => {
             <img className="checkmark" src='./img/rr/checkmark.svg' />
             I recommend this product
           </p> : '' }
-      <p>[not working yet] Was this review helpful? <button>Yes</button> <button>No</button></p>
+      {props.review.reponse ? <div className="review-response"><p><strong>Response</strong></p><p>{props.review.response}</p></div> : ''}
+      { props.helpfulReviews.includes(props.review.review_id) ? '' :
+        <p>
+          Was this review helpful?
+          <button
+            onClick={() => {
+              props.helpful(props.review.review_id)
+              .then(() => props.setHelpful([...props.helpfulReviews, props.review.review_id]));
+            }}
+          >
+            Yes
+          </button> ({props.review.helpfulness})
+          {/* BRD mentions a 'No' button, but the API doesn't retain or allow posts to this value, deferring to mockup */}
+          <span
+            className="report"
+            onClick={({target}) => {
+              props.reportReview(props.review.review_id);
+              target.parentElement.parentElement.classList.toggle('hidden');
+            }}
+          >
+            Report
+          </span>
+        </p>
+      }
     </div>
   )
 };
@@ -396,34 +476,50 @@ let PercentWidget = (props) => (
 );
 
 let RatingBreakDown = (props) => {
-  let keys = props.meta ? (props.meta.ratings ? Object.keys(props.meta.ratings).reverse() : []) : [];
-  return (
-    <div className="rating-breakdown">
-      <h2>Rating Breakdown</h2>
-      <h1>{props.average.toFixed(1)}<sup><StarRating rating={props.average} /></sup></h1>
-      {keys.map(i =>
-        <PercentWidget
-          stars={i}
-          key={i}
-          percent={parseInt(props.meta.ratings[i]) / props.total * 100}
-          toggleFilter={props.toggleFilter}
-          ratingFilter={props.ratingFilter}
-        />
-      )}
-    </div>
-  )
+  //let keys = props.meta ? (props.meta.ratings ? Object.keys(props.meta.ratings).reverse() : []) : [];
+  let keys = [1, 2, 3, 4, 5];
+  //console.log(props.meta);
+  if (props.reviews.length) {
+    return (
+      <div className="rating-breakdown">
+        <h2>Rating Breakdown</h2>
+        <h1>{props.average.toFixed(1)}<sup><StarRating rating={props.average} /></sup></h1>
+        <p>{
+          props.meta.recommended ?
+            Math.round(Number(props.meta.recommended.true) * 100 / (Number(props.meta.recommended.true) + Number(props.meta.recommended.false)))
+            : 0}
+          % of reviews recommended this product
+        </p>
+        {props.meta.ratings ? keys.map(i =>
+          <PercentWidget
+            stars={i}
+            key={i}
+            percent={parseInt(props.meta.ratings[i] || 0) / props.total * 100}
+            toggleFilter={props.toggleFilter}
+            ratingFilter={props.ratingFilter}
+          />
+        ) : ''}
+      </div>
+    )
+  } else {
+    return <div className='rating-breakdown'><p>No reviews yet</p></div>
+  }
 };
 
 let ProductBreakDown = (props) => {
   let keys = props.meta ? (props.meta.characteristics ? Object.keys(props.meta.characteristics) : []) : [];
-  return (
-    <div className="product-breakdown">
-      <h2>Product Breakdown</h2>
-      {keys.map((char,i) =>
-        <CharacteristicsWidget key={i} name={char} />
-      )}
-    </div>
-  )
+  if (props.reviews.length) {
+    return (
+      <div className="product-breakdown">
+        <h2>Product Breakdown</h2>
+        {keys.map((char,i) =>
+          <CharacteristicsWidget key={i} name={char} />
+        )}
+      </div>
+    )
+  } else {
+    return <div className='product-breakdown'><p>No reviews yet</p></div>
+  }
 };
 let CharacteristicsWidget = (props) => {
   let text = {
@@ -452,7 +548,7 @@ let CharacteristicsWidget = (props) => {
 let SortOptions = (props) => (
 <div className="sort-options">
   <div>
-    {props.total} reviews, sorted by <select onChange={({target}) => props.setSort(target.value)}>
+    {!isNaN(props.total) ? props.total : 0} reviews, sorted by <select onChange={({target}) => props.setSort(target.value)}>
       <option value="related">relevance</option>
       <option value="helpful">helpfulness</option>
       <option value="newest">newest</option>
@@ -464,14 +560,97 @@ let SortOptions = (props) => (
 </div>
 );
 
-let WriteReview = (props) => (
-  <div className="hidden write-review">
-    <p>Write Review</p>
-    <p
+let WriteReview = (props) => {
+  let characteristics = {
+    Size: {
+      1: 'A size too small', 
+      2: '1/2 a size too small',
+      3: 'Perfect',
+      4: '1/2 a size too big',
+      5: 'A size too wide'
+    },
+    Width: {
+      1: 'Too narrow',
+      2: 'Slightly narrow',
+      3: 'Perfect',
+      4: 'Slightly wide',
+      5: 'Too wide'
+    },
+    Comfort: {
+      1: 'Uncomfortable',
+      2: 'Slightly uncomfortable',
+      3: 'Ok',
+      4: 'Comfortable',
+      5: 'Perfect'
+    },
+    Quality: {
+      1: 'Poor',
+      2: 'Below Average',
+      3: 'What I expected',
+      4: 'Pretty great',
+      5: 'Perfect'
+    },
+    Length: {
+      1: 'Runs short',
+      2: 'Below average',
+      3: 'What I expected',
+      4: 'Runs slightly long',
+      5: 'Runs long'
+    },
+    Fit: {
+      1: 'Runs tight',
+      2: 'Runs slightly tight',
+      3: 'Perfect',
+      4: 'Runs slightly long',
+      5: 'Runs long'
+    }
+  }
+  let keys = Object.keys(characteristics);
+  return (
+    <div className="hidden write-review">
+      <div className='close'
       style={{color: 'blue', textDecoration: 'underline'}}
       onClick={() => document.getElementsByClassName('write-review')[0].classList.toggle('hidden')}
-    >close</p>
-  </div>
-);
+    >
+      Cancel Review (close)
+      </div>
+      <h1>Write Review</h1>
+      <h2>Overall rating (mandatory)</h2>
+      <p>[interactive stars with highlight-on-hover will go here] 1 - poor, 2 - fair, 3 - average, 4 - good, 5 - great </p>
+      <h2>Do you recommend this product? (mandatory)</h2>
+      <p><input type="radio" name="recommend" value="true" /> Yes</p>
+      <p><input type="radio" name="recommend" value="false" /> No</p>
+      <h2>Characteristics (mandatory)</h2>
+      <table>
+        <tbody>
+        {
+          keys.map((k, i) => 
+            <tr key={i}>
+              <td><strong>{k}</strong></td>
+              {[1, 2, 3, 4, 5].map(field => 
+                <td key={field}>{characteristics[k][field]} <input type='radio' name={k} value={field} /></td>
+              )}
+            </tr>
+          )
+        }
+        </tbody>
+      </table>
+      <h2>Review Summary (mandatory)</h2>
+      <p>[limit to 60 characters]</p>
+      <input type="text" name="review-summary" placeholder="Example: Best purchase ever!" />
+      <h2>Review body (mandatory)</h2>
+      <p>[minimum 50, max 1000 characters]</p>
+      <textarea rows="24" cols="80" name="review-body" ></textarea>
+      <h2>Upload your photos</h2>
+      <p>[this is gonna be tricky]</p>
+      <h2>Your email (mandatory)</h2>
+      <p>[up to 60 characters]</p>
+      <input type="email" placeholder="Example: jackson11@email.com" />
+      <p>For authentication reasonse, you will not be emailed.</p>
+      <button>Submit Review</button>
+      <p>[check that for blank mandatory fields, review body [50, 1000] in length, proper email format, and valid images selected]</p>
+    </div>
+  )
+};
 
 export default ReviewsAndRatings;
