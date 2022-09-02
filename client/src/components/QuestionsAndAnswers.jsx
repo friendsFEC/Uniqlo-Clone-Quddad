2a7edb193ca81f0e684c640baaa535a642fec003
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-01 20:20:51 -0700

    Update QuestionsAndAnswers.jsx
    Switching from static data to API data

--- client/src/components/QuestionsAndAnswers.jsx
+++ client/src/components/QuestionsAndAnswers.jsx
@@ -4,6 +4,17 @@ import Filter from './QA components/Filter.jsx';
 import ListOfQA from './QA components/ListOfQA.jsx';
 import QAEntry from './QA components/QAEntry.jsx';
 import example from './QA components/example.jsx';
+import config from '../../../config.js'
+
+
+const baseURL = 'https://app-hrsei-api.herokuapp.com/api/fec2/rfp';
+const qaURL = `${baseURL}/qa/questions`;
+const Axios = axios.create({
+  baseURL: 'https://app-hrsei-api.herokuapp.com/api/fec2/rfp/',
+  headers: {
+    Authorization: config.API_KEY,
+  },
+});
 
 class QuestionsAndAnswers extends React.Component {
   constructor(props) {
@@ -11,13 +22,34 @@ class QuestionsAndAnswers extends React.Component {
 
     this.state = {
       product: example.results,
-      product_id: this.props.productId,
+      product_id: '65632',
     };
   }
 
   componentDidMount() {
-    console.log('product id ', this.state.product_id);
+    const chosenProductId = `/?product_id=${this.state.product_id}`;
+    // Make a request for a user with a given ID
 
+    // Make a request for a user with a given ID
+    const get_product_url = qaURL + chosenProductId;
+    const result = Axios.get(get_product_url, {
+      transformResponse: [(data) => {
+        data = JSON.parse(data);
+        console.log(data);
+      }],
+    });
+    // axios.get('/user?ID=12345')
+    //   .then(function (response) {
+    //     // handle success
+    //     console.log(response);
+    //   })
+    //   .catch(function (error) {
+    //     // handle error
+    //     console.log(error);
+    //   })
+    //   .then(function () {
+    //     // always executed
+    //   });
   }
 
   render() {
