8e7af418033194c6da59bb4250bb0ab5a4e84468
James Stolhammer <jimstolhammer@gmail.com>
2022-09-03 10:31:11 -0700

    De-linted `SortOptions.jsx`

--- client/src/components/rr/SortOptions.jsx
+++ client/src/components/rr/SortOptions.jsx
@@ -1,19 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function SortOptions(props) {
+export default function SortOptions({
+  total, setSort, search, setSearch,
+}) {
+  const displayTotal = !Number.isNaN(total) ? total : 0;
   return (
     <div className="sort-options">
       <div>
-        {!isNaN(props.total) ? props.total : 0} reviews, sorted by <select onChange={({target}) => props.setSort(target.value)}>
+        {`${displayTotal} reviews, sorted by`}
+        <select onChange={({ target }) => setSort(target.value)}>
           <option value="related">relevance</option>
           <option value="helpful">helpfulness</option>
           <option value="newest">newest</option>
         </select>
-        Search Reviews:<input type="search" value={props.search} onChange={({target}) => {
-          props.setSearch(target.value);
-        }}/>
+        Search Reviews:
+        <input
+          type="search"
+          value={search}
+          onChange={({ target }) => {
+            setSearch(target.value);
+          }}
+        />
       </div>
     </div>
-  )
+  );
+}
+
+SortOptions.propTypes = {
+  total: PropTypes.number.isRequired,
+  setSort: PropTypes.func.isRequired,
+  search: PropTypes.string.isRequired,
+  setSearch: PropTypes.func.isRequired,
 };
