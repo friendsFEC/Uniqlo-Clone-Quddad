d72e3a3501a9765bb4643231f199d6da1c7479a7
James Stolhammer <jimstolhammer@gmail.com>
2022-09-03 08:57:09 -0700

    Delinted `ProductBreakDown.jsx`

--- client/src/components/rr/ProductBreakDown.jsx
+++ client/src/components/rr/ProductBreakDown.jsx
@@ -1,41 +1,127 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-export default function ProductBreakDown(props) {
-  const keys = props.meta ? (props.meta.characteristics ? Object.keys(props.meta.characteristics) : []) : [];
-  if (props.reviews.length) {
+function ProductBreakDown({ meta, reviews }) {
+  let keys = [];
+  if (meta.characteristics) {
+    keys = keys.concat(Object.keys(meta.characteristics));
+  }
+  if (reviews.length) {
     return (
       <div className="product-breakdown">
         <h2>Product Breakdown</h2>
-        {keys.map((char,i) =>
-          <CharacteristicsWidget key={i} name={char} />
-        )}
+        { keys.map((char) => (
+          <CharacteristicsWidget
+            key={`characteristic-${char}`}
+            name={char}
+          />
+        )) }
       </div>
-    )
-  } else {
-    return <div className='product-breakdown'><p>No reviews yet</p></div>
+    );
   }
+  return <div className="product-breakdown"><p>No reviews yet</p></div>;
+}
+
+ProductBreakDown.propTypes = {
+  meta: PropTypes.shape(
+    {
+      product_id: PropTypes.string,
+      ratings: PropTypes.shape(
+        {
+          1: PropTypes.string,
+          2: PropTypes.string,
+          3: PropTypes.string,
+          4: PropTypes.string,
+          5: PropTypes.string,
+        },
+      ),
+      characteristics: PropTypes.arrayOf(
+        {
+          Fit: PropTypes.shape(
+            {
+              id: PropTypes.number,
+              value: PropTypes.string,
+            },
+          ),
+          Width: PropTypes.shape(
+            {
+              id: PropTypes.number,
+              value: PropTypes.string,
+            },
+          ),
+          Comfort: PropTypes.shape(
+            {
+              id: PropTypes.number,
+              value: PropTypes.string,
+            },
+          ),
+          Quality: PropTypes.shape(
+            {
+              id: PropTypes.number,
+              value: PropTypes.string,
+            },
+          ),
+          Length: PropTypes.shape(
+            {
+              id: PropTypes.number,
+              value: PropTypes.string,
+            },
+          ),
+          Size: PropTypes.shape(
+            {
+              id: PropTypes.number,
+              value: PropTypes.string,
+            },
+          ),
+        },
+      ),
+    },
+  ).isRequired,
+  reviews: PropTypes.arrayOf(
+    PropTypes.shape(
+      {
+        review_id: PropTypes.number,
+        rating: PropTypes.number,
+        summary: PropTypes.string,
+        recommend: PropTypes.bool,
+        body: PropTypes.string,
+        date: PropTypes.string,
+        reviewer_name: PropTypes.string,
+        helpfulness: PropTypes.number,
+        photos: PropTypes.arrayOf(
+          PropTypes.string,
+        ),
+      },
+    ),
+  ).isRequired,
 };
-let CharacteristicsWidget = (props) => {
-  let text = {
-    Fit: {left: 'Too tight', middle: 'Perfect', right: 'Too loose'},
-    Comfort: {left: 'Uncomfortable', middle: 'Average', right: 'Comfortable'},
-    Length: {left: 'Too short', middle: 'Perfect', right: 'Too long'},
-    Quality: {left: 'Poor', middle: 'Average', right: 'Great'},
-    Size: {left: 'Too small', middle: 'Just right', right: 'Too large'},
-    Width: {left: 'Too thin', middle: 'Just right', right: 'Too wide'},
-  }
+
+function CharacteristicsWidget({ name }) {
+  const text = {
+    Fit: { left: 'Too tight', middle: 'Perfect', right: 'Too loose' },
+    Comfort: { left: 'Uncomfortable', middle: 'Average', right: 'Comfortable' },
+    Length: { left: 'Too short', middle: 'Perfect', right: 'Too long' },
+    Quality: { left: 'Poor', middle: 'Average', right: 'Great' },
+    Size: { left: 'Too small', middle: 'Just right', right: 'Too large' },
+    Width: { left: 'Too thin', middle: 'Just right', right: 'Too wide' },
+  };
   return (
-    <div className='characteristics'>
-      {props.name}
-      <div className='characteristics-bar'>
-        <img className='characteristics-arrow' src='./img/rr/arrow.svg' />
+    <div className="characteristics">
+      {name}
+      <div className="characteristics-bar">
+        <img alt="" className="characteristics-arrow" src="./img/rr/arrow.svg" />
       </div>
-      <div className='characteristics-description'>
-        <p className='rr-left'>{text[props.name].left}</p>
-        <p className='rr-middle'>{text[props.name].middle}</p>
-        <p className='rr-right'>{text[props.name].right}</p>
+      <div className="characteristics-description">
+        <p className="rr-left">{text[name].left}</p>
+        <p className="rr-middle">{text[name].middle}</p>
+        <p className="rr-right">{text[name].right}</p>
       </div>
     </div>
-  )
+  );
 }
+
+CharacteristicsWidget.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default ProductBreakDown;
