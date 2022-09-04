46a18c8db665245700b091d3392100b2453c3564
Goksu Okar <goksuokar@gmail.com>
2022-09-04 12:44:27 -1000

    drop down quantity goes back to dash when default selected

--- client/src/components/overview/SizeAndQuantity.jsx
+++ client/src/components/overview/SizeAndQuantity.jsx
@@ -1,21 +1,26 @@
 // currentstyle.skus > each key is sku id and each sku is an object
 // goes xs s m l xl xxl
 // 65632 has out of stuck > sku key will say 'null'
-import _, { map } from 'underscore';
+import _ from 'underscore';
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 // sizes is an object of skus passed from Overview
 
 function SizeAndQuantity({ sizes }) {
-  const [sizeId, setSizeId] = useState('');
+  const [sizeId, setSizeId] = useState('noSize');
+  let quantity = 0;
 
   const changeSize = (option) => {
     const newId = option.value;
     setSizeId(newId);
   };
 
-  console.log(sizes[sizeId]);
+  if (sizeId !== 'noSize') {
+    quantity = sizes[sizeId].quantity > 15 ? 16 : sizes[sizeId].quantity + 1;
+  } else {
+    quantity = 0;
+  }
 
   // quantity is - if no size selected 1 if size selected
   if (sizes.null) {
@@ -25,13 +30,13 @@ function SizeAndQuantity({ sizes }) {
   return (
     <div>
       <select onChange={({ target }) => changeSize(target)}>
-        <option>size</option>
+        <option value="noSize">Select Size</option>
         {_.map(sizes, (property, id) => (
           <option value={id}>{property.size}</option>
         ))}
       </select>
       <select>
-        {sizeId === '' ? <option>-</option> : _.range(1, sizes[sizeId].quantity).map((num) => (
+        {sizeId === 'noSize' ? <option>-</option> : _.range(1, quantity).map((num) => (
           <option value={num}>{num}</option>
         ))}
       </select>
@@ -44,5 +49,3 @@ SizeAndQuantity.propTypes = {
 };
 
 export default SizeAndQuantity;
-
-
