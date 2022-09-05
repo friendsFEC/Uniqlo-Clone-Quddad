7b80c30da92e4dffcbdc3afefef0d2e78d30d898
jamesEmerson112 <36806380+jamesEmerson112@users.noreply.github.com>
2022-09-05 13:28:13 -0600

    Update ListOfQA.jsx
    After the user types 3 or more characters into the search bar the results will begin to filter to only those containing matching text. The filter should continue to update as the user adds or removes characters.

--- client/src/components/QA components/ListOfQA.jsx
+++ client/src/components/QA components/ListOfQA.jsx
@@ -1,43 +1,47 @@
 import React from 'react';
 import QAItem from './QAItem.jsx';
 import Filter from './Filter.jsx';
+
 const _V = require('../Utility/V.jsx');
 
 class ListOfQA extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      chosenProduct: this.props.chosenProduct
+      chosenProduct: this.props.chosenProduct,
     };
 
-    this.searchQuestions   = this.searchQuestions.bind(this);
-    this.filteredQuestions = this.filterQuestions.bind(this);
+    this.searchQuestions = this.searchQuestions.bind(this);
+    this.filterQuestions = this.filterQuestions.bind(this);
   }
 
   searchQuestions(event) {
-    const searchedQuestions = event.target.value;
-    const newChosenProduct = this.filterQuestions(searchedQuestions, this.props.chosenProduct);
+    let searchedQuestionsStr = event.target.value;
+    searchedQuestionsStr = searchedQuestionsStr.length >= 3 ? searchedQuestionsStr : '';
+    const { chosenProduct } = this.props;
+    const newChosenProduct = this.filterQuestions(searchedQuestionsStr, chosenProduct);
     this.setState({
-      chosenProduct: newChosenProduct
-    })
+      chosenProduct: newChosenProduct,
+    });
   }
 
   filterQuestions(searchedStr, questions) {
     searchedStr = searchedStr || '';
+    // if (searchedStr.length < 3) {
+    //   return;
+    // }
     let results = [];
     const regExpConst = new RegExp(`${searchedStr}`, 'gi');
     results = questions.filter((question) => {
       const questionBody = question.question_body;
       return questionBody.match(regExpConst) !== null;
-    })
+    });
     return results;
   }
 
   render() {
     // filter out the questions that are NOT helpful
-    const filteredQuestions = this.state.chosenProduct.filter((question) =>
-      question.question_helpfulness > 0
-    );
+    const filteredQuestions = this.state.chosenProduct.filter((question) => question.question_helpfulness > 0);
 
     // display the first 4 questions
     const topFourQuestions = _V.topXItems(4, filteredQuestions);
@@ -46,16 +50,13 @@ class ListOfQA extends React.Component {
     return (
       <div id="qa-ListOfQA">
 
-      <Filter searchQuestions={this.searchQuestions}/>
-      {/*
+        <Filter searchQuestions={this.searchQuestions} />
+        {/*
       1. display questions
       2. display answers */}
-      {topFourQuestions.map((item) => {
-        return <QAItem key={item.question_id} questionAnswer={item} />
-      })}
+        {topFourQuestions.map((item) => <QAItem key={item.question_id} questionAnswer={item} />)}
       </div>
-    )
-  };
-
+    );
+  }
 }
 export default ListOfQA;
