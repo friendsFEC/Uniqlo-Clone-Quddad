479a559d0d392879aeceed14c0b3f524975e8513
Nick <nicolastiennguyen@gmail.com>
2022-09-05 16:17:04 -0700

    removed comments

--- client/src/components/RC/Modal.jsx
+++ client/src/components/RC/Modal.jsx
@@ -4,22 +4,6 @@ import React from 'react';
 const Modal = ({ open, onClose, selectedProductID, productID, product, currentInfo}) => {
   if (!open) return null;
 
-  // const allFeatures = [];
-  // allFeatures.push(currentInfo.features);
-  // allFeatures.push(product.features);
-  // const mergedFeatures = [].concat.apply([], allFeatures);
-  // console.log(mergedFeatures)
-  // const filteredFeatures = {};
-  // mergedFeatures.forEach(item => {
-  //   if (!filteredFeatures.hasOwnProperty(item[feature])) {
-  //     filteredFeatures[]
-  //   }
-  //   console.log(item, 'ITEM')
-  //   console.log(item.feature, 'ITEM[FEATURE]')
-  // })
-
-
-
   if (selectedProductID === product.id) {
     return (
     <div className = "rc-modal-style">
@@ -44,20 +28,15 @@ const Modal = ({ open, onClose, selectedProductID, productID, product, currentIn
               </tr>
             } else {
             return <tr key = {index}>
-            <td>{feature.value || 'N/A'}</td>
-            <td>{feature.feature}</td>
-            <td>{'N/A'}</td>
+              <td>{feature.value || 'N/A'}</td>
+              <td>{feature.feature}</td>
+              <td>{'N/A'}</td>
             </tr>
             }
           })}
           {product.features.map((feature, index) => {
             if (currentInfo.features[index] && currentInfo.features[index].feature === feature.feature) {
-              // return <tr key = {index}>
-              //   <td>{currentInfo.features[index].value || 'N/A'}</td>
-              //   <td>{feature.feature}</td>
-              //   <td>{feature.value || 'N/A'}</td>
-              // </tr>
-              return null
+              return null;
             } else {
             return <tr key = {index}>
               <td>{'N/A'}</td>
@@ -76,6 +55,3 @@ const Modal = ({ open, onClose, selectedProductID, productID, product, currentIn
 };
 
 export default Modal;
-
-// inside the modal table line 38 area
-// if the current info's feature matches the selected product's feature, then we will render the value of the selected product
