5f0af46655ed69ecaf94cc594795de1591457d39
Nick <nicolastiennguyen@gmail.com>
2022-09-05 18:15:18 -0700

    EOD Commit

--- client/src/components/RC/YOCard.jsx
+++ client/src/components/RC/YOCard.jsx
@@ -6,26 +6,17 @@ import Stars from './Stars.jsx'
 const YOCard = ({open, currentInfo, currentStyle, currentRating, relatedAverageRatings}) => {
   if (!open) return null
 
-  const removeDiv = () => {
-    const getDiv = document.getElementById("rc-yo-new-card")
-    if (getDiv.style.display === "none") {
-      getDiv.style.display = "block";
-    } else {
-      getDiv.style.display = "none";
-    }
-  }
-
   return (
-    <div id = "rc-yo-new-card">
-      <div className = "rc-yo-card-details">
-        <button className = "rc-yo-remove-button" onClick = {() => removeDiv()}><AiOutlineCloseCircle/></button>
-        <img className = "rc-card-photos" src = {currentStyle.results[0].photos[0].thumbnail_url}/>
-        <p>{currentInfo.category}</p>
-        <p className = "rc-card-name">{currentInfo.name}</p>
-        <p>{currentInfo.default_price}</p>
-        < Stars currentRating = {currentRating} relatedAverageRatings = {relatedAverageRatings}/>
+      <div className = "rc-yo-card">
+        <div className = "rc-yo-card-details">
+          <button className = "rc-yo-remove-button" ><AiOutlineCloseCircle/></button>
+          <img className = "rc-card-photos" src = {currentStyle.results[0].photos[0].thumbnail_url}/>
+          <p>{currentInfo.category}</p>
+          <p className = "rc-card-name">{currentInfo.name}</p>
+          <p>{currentInfo.default_price}</p>
+          < Stars currentRating = {currentRating} relatedAverageRatings = {relatedAverageRatings}/>
+        </div>
       </div>
-    </div>
   )
 }
 
--- client/src/components/RC/YourOutfit.jsx
+++ client/src/components/RC/YourOutfit.jsx
@@ -7,12 +7,15 @@ import { GrFormNext, GrFormPrevious } from 'react-icons/gr';
 const YourOutfit = ( {currentInfo, currentStyle, currentRating, relatedAverageRatings} ) => {
   const [isOpen, setIsOpen] = useState(false);
   const [infoStorage, setInfoStorage] = useState([]);
+  // const [styleStorage, setStyleStorage] = useState([]);
 
   // when press "add to your outfit", add the current info to added info
   const addedInfo = [];
 
   const removeDiv = () => {
-    const getDiv = document.getElementsByClassName("rc-yo-card")
+    // const getDiv = document.getElementsByClassName("rc-yo-card")
+    // const getDiv = document.getElementById("rc-yo-add-button-div")
+    const getDiv = document.getElementById("test")
     if (getDiv.style.display === "none") {
       getDiv.style.display = "block";
     } else {
@@ -20,13 +23,18 @@ const YourOutfit = ( {currentInfo, currentStyle, currentRating, relatedAverageRa
     }
   }
 
+  // in YOCard, do I need to now pass in the mapped product's information? probably yes. so currentInfo would need to be product's current info, etc.
+  // how would we do this? --> keep track of all "added information" in state of this component
+  // so this state would be updated as we cycle through products and add them to your outfit by clicking on the button
+  // then we render your outfit based on this state
+
   return (
     <div>
       <h3 className = "rc-title"> Your Outfit</h3>
-        <div className = "rc-yo-container">
+      <div className = "rc-yo-container">
         {/* < GrFormPrevious className = "rc-rp-arrow"/> */}
 
-
+        <div id = "test">
           <div className = "rc-yo-card">
             <div id = "rc-yo-add-button-div">
               <button className = "rc-yo-add-button" onClick = {() => {
@@ -36,26 +44,28 @@ const YourOutfit = ( {currentInfo, currentStyle, currentRating, relatedAverageRa
                 setInfoStorage(addedInfo);
               }}>Add to Your Outfit</button>
             </div>
-            <YOCard open = {isOpen} currentInfo = {currentInfo} currentStyle = {currentStyle} currentRating = {currentRating} relatedAverageRatings = {relatedAverageRatings} />
+            {/* <YOCard open = {isOpen} currentInfo = {currentInfo} currentStyle = {currentStyle} currentRating = {currentRating} relatedAverageRatings = {relatedAverageRatings} /> */}
+
           </div>
-
-
-
-          {isOpen ?
-          <div className = "rc-yo-card">
-            <div id = "rc-yo-add-button-div">
-              <button className = "rc-yo-add-button" onClick = {() => {
-              alert("Error: This product is already apart of Your Outfit!")}}>Add to Your Outfit</button>
-            </div>
-          </div> : null}
-
-
-
-
-
-
-          {/* < GrFormNext className = "rc-rp-arrow"/> */}
         </div>
+
+        {infoStorage.map((product, index) => {
+          return (<div key = {index}>
+            <YOCard open = {isOpen} currentInfo = {currentInfo} currentStyle = {currentStyle} currentRating = {currentRating} relatedAverageRatings = {relatedAverageRatings}/>
+            </div>
+          )
+        })}
+
+        {isOpen ?
+        <div className = "rc-yo-card">
+          <div id = "rc-yo-add-button-div">
+            <button className = "rc-yo-add-button" onClick = {() => {
+            alert("This product is already apart of Your Outfit!")}}>Add to Your Outfit</button>
+          </div>
+        </div> : null}
+
+      {/* < GrFormNext className = "rc-rp-arrow"/> */}
+      </div>
     </div>
   )
 }
